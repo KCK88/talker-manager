01f819cb8775968ce7912e1f69ff283c0c2e4eac
Claudio Meira <claudiomeirabs@gmail.com>
2024-06-21 15:54:43 -0300

    Iniciando projeto e realizando requisito 01

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(express.json());
@@ -9,6 +10,20 @@ const PORT = process.env.PORT || '3001';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (_req, res) => {
+  try {
+    const fileContent = await fs.readFile('./src/talker.json', 'utf8');
+    const talkers = JSON.parse(fileContent);
+
+    if (!talkers.length > 0) {
+      return res.status(HTTP_OK_STATUS).json([]);
+    }
+    res.status(HTTP_OK_STATUS).json(talkers);
+  } catch (error) {
+    return res.status(HTTP_OK_STATUS).json([]);
+  }
 });
 
 app.listen(PORT, () => {
